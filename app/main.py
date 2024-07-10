bf80e96a53d63ec81de2f94558b8572da1a3dfe8
LuizaAlex <poteca.luiza@gmail.com>
2024-07-10 21:24:11 +0300

    Step 2 implemented

--- app/main.py
+++ app/main.py
@@ -1,15 +1,58 @@
 
 import socket
 
+def handle_request(request):
+    #Split request into lines
+    lines = request.split('\r\n')
+
+    #Extract the request line
+    request_line = lines[0]
+
+    #Extract the method, path, and HTTP version
+    method, path, http_version = request_line.split(' ')
+
+    #Generate response based on the path
+
+    if path == '/': 
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    return response
+
 
 def main():
    
     print("Logs from your program will appear here!")
 
-    
+    # Create a server socket
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print("Server running on localhost:4221")
+
+    while True:
+
+        #Accept a connection 
+        client_socket, client_address = server_socket.accept()
+
+        #Receive the request
+        request = client_socket.recv(1024).decode('utf-8')
+        print(f"Request: {request}")
+
+        #Handle the response and generate a response
+        response = handle_request(request)
+        
+        #Send the response
+        client_socket.sendall(response.encode('utf-8'))
+
+        #Close the connection
+        client_socket.close()
+
+
+
+
+
     #server_socket.accept() # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    #server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
